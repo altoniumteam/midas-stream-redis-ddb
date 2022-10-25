e0e72785333ee0d7bacdcd3bfc99421aabf074e6
ShunShuuS <111737409+ShunShuuS@users.noreply.github.com>
2022-10-25 09:51:04 +0700

    commit

--- functions/midas-stream.js
+++ functions/midas-stream.js
@@ -1,44 +1,48 @@
-const { createClient } = require('redis');
-const { load, dumpPrettyText } = require('ion-js');
-const { deaggregateSync } = require('aws-kinesis-agg');
-const { debug } = require('@dazn/lambda-powertools-logger');
-const { updateBalance, getWsConnectionId } = require("./helper/midas-ddb-helper");
+const { createClient } = require("redis");
+const { load, dumpPrettyText } = require("ion-js");
+const { deaggregateSync } = require("aws-kinesis-agg");
+const { debug } = require("@dazn/lambda-powertools-logger");
+const {
+  updateBalance,
+  getWsConnectionId,
+} = require("./helper/midas-ddb-helper");
 const { sendWebsocketMessage } = require("./helper/update-balance-websocket");
 const { sendMessage } = require("./helper/midas-sqs");
 
 let client;
 
-if (typeof client === 'undefined') {
+if (typeof client === "undefined") {
   client = createClient({
-      url: 'redis://midascache.c7fgoy.ng.0001.apse1.cache.amazonaws.com:6379'
+    url: "redis://midascache.c7fgoy.ng.0001.apse1.cache.amazonaws.com:6379",
   });
-  client.connect()
-}  else {
-  console.log('socket masih kebuka')
+  client.connect();
+} else {
+  console.log("socket masih kebuka");
 }
 
 const computeChecksums = true;
 
 const JournalUrl = process.env.JournalUrl;
 const StatisticUrl = process.env.StatisticUrl;
+const BonusSystemUrl = process.env.BonusSystemUrl;
 
 const redisCache = async (client, key, value) => {
-    console.log("START", value);
-    let result
-    try {
-      client.on('error', (err) => console.log("Redis error", err));
-      
-      result = await client.get(key);
+  console.log("START", value);
+  let result;
+  try {
+    client.on("error", (err) => console.log("Redis error", err));
 
-      if (result == null || result == undefined) {
-        result = await client.set(key, value)         
-      };
+    result = await client.get(key);
 
-      return result;
-    } catch(err) {
-      console.log(err);
-      return err;
-    };
+    if (result == null || result == undefined) {
+      result = await client.set(key, value);
+    }
+
+    return result;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 };
 
 const promiseDeaggregate = (record) =>
@@ -61,8 +65,7 @@ async function processIon(ionRecord) {
 
   // debug(`Version ${version} and id ${id}`);
 
-
-  console.log('ionRecord: ' + JSON.stringify(ionRecord));
+  console.log("ionRecord: " + JSON.stringify(ionRecord));
 
   // Check to see if the data section exists.
   if (ionRecord == null || !ionRecord) {
@@ -70,68 +73,37 @@ async function processIon(ionRecord) {
     //delete
   } else {
     // if (ionRecord.payload.tableInfo.tableName.stringValue() == "playerWallet") {
-    const brandUsername =
-      ionRecord.brandUsername;
-    const brandId = 
-      ionRecord.brandId;
-    const username = 
-      ionRecord.username;
-    const previousBalance =
-      ionRecord.previousBalance;
-    const currentBalance =
-      ionRecord.currentBalance;
-    const bonusCurrentBalance =
-      ionRecord.bonusCurrentBalance;
-    const bonusPreviousBalance =
-      ionRecord.bonusPreviousBalance;
-    const bonusAdjustAmount =
-      ionRecord.bonusAdjustAmount;
-    const adjustAmount =
-      ionRecord.adjustAmount;
-    const txType =
-      ionRecord.txType;
-    const txTypeAtt1 =
-      ionRecord.txTypeAtt1;
-    const txTypeAtt2 =
-      ionRecord.txTypeAtt2;
-    const txTypeAtt3 =
-      ionRecord.txTypeAtt3;
-    const gameId =
-      ionRecord.metadata.gameId;
-    const providerId =
-      ionRecord.metadata.providerName;
-    const reference =
-      ionRecord.metadata.reference;
-    const roundDetails =
-      ionRecord.metadata.roundDetails;
-    const roundId =
-      ionRecord.metadata.roundId;
-    const bonusCode =
-      ionRecord.metadata.bonusCode;
-    const gpTimestamp =
-      ionRecord.metadata.gpTimestamp;
-    const createdAt = 
-      ionRecord.createdAt;
-    const usedPromo =
-      ionRecord.metadata.usedPromo;
-    const validBetAmount =
-      ionRecord.validBetAmount;
-    const promoWinAmount =
-      ionRecord.metadata.promoWinAmount;
-    const jackpotId =
-      ionRecord.metadata.jackpotId;
-    const promoCampaignType =
-      ionRecord.metadata.promoCampaignType;
-    const promoCampaignId =
-      ionRecord.metadata.promoCampaignId;
-    const promoWinReference =
-      ionRecord.metadata.promoWinReference;
-    const campaignType =
-      ionRecord.metadata.campaignType;
-    const campaignId =
-      ionRecord.metadata.campaignId;
-    const statistic =
-      ionRecord.statistic;
+    const brandUsername = ionRecord.brandUsername;
+    const brandId = ionRecord.brandId;
+    const username = ionRecord.username;
+    const previousBalance = ionRecord.previousBalance;
+    const currentBalance = ionRecord.currentBalance;
+    const bonusCurrentBalance = ionRecord.bonusCurrentBalance;
+    const bonusPreviousBalance = ionRecord.bonusPreviousBalance;
+    const bonusAdjustAmount = ionRecord.bonusAdjustAmount;
+    const adjustAmount = ionRecord.adjustAmount;
+    const txType = ionRecord.txType;
+    const txTypeAtt1 = ionRecord.txTypeAtt1;
+    const txTypeAtt2 = ionRecord.txTypeAtt2;
+    const txTypeAtt3 = ionRecord.txTypeAtt3;
+    const gameId = ionRecord.metadata.gameId;
+    const providerId = ionRecord.metadata.providerName;
+    const reference = ionRecord.metadata.reference;
+    const roundDetails = ionRecord.metadata.roundDetails;
+    const roundId = ionRecord.metadata.roundId;
+    const bonusCode = ionRecord.metadata.bonusCode;
+    const gpTimestamp = ionRecord.metadata.gpTimestamp;
+    const createdAt = ionRecord.createdAt;
+    const usedPromo = ionRecord.metadata.usedPromo;
+    const validBetAmount = ionRecord.validBetAmount;
+    const promoWinAmount = ionRecord.metadata.promoWinAmount;
+    const jackpotId = ionRecord.metadata.jackpotId;
+    const promoCampaignType = ionRecord.metadata.promoCampaignType;
+    const promoCampaignId = ionRecord.metadata.promoCampaignId;
+    const promoWinReference = ionRecord.metadata.promoWinReference;
+    const campaignType = ionRecord.metadata.campaignType;
+    const campaignId = ionRecord.metadata.campaignId;
+    const statistic = ionRecord.statistic;
 
     debug("*** playerWallet Table, execute! ***");
     debug(brandUsername);
@@ -175,24 +147,19 @@ async function processIon(ionRecord) {
       txTypeAtt1,
       txTypeAtt2,
       txTypeAtt3,
-      gameId:
-        txTypeAtt1 == "Deposit" || txTypeAtt1 == "Withdraw" ? "" : gameId,
+      gameId: txTypeAtt1 == "Deposit" || txTypeAtt1 == "Withdraw" ? "" : gameId,
       providerId:
         txTypeAtt1 == "Deposit" || txTypeAtt1 == "Withdraw" ? "" : providerId,
       reference:
         txTypeAtt1 == "Deposit" || txTypeAtt1 == "Withdraw" ? "" : reference,
       roundDetails:
-        txTypeAtt1 == "Deposit" || txTypeAtt1 == "Withdraw"
-          ? ""
-          : roundDetails,
+        txTypeAtt1 == "Deposit" || txTypeAtt1 == "Withdraw" ? "" : roundDetails,
       roundId:
         txTypeAtt1 == "Deposit" || txTypeAtt1 == "Withdraw" ? "" : roundId,
       bonusCode:
         txTypeAtt1 == "Deposit" || txTypeAtt1 == "Withdraw" ? "" : bonusCode,
       gpTimestamp:
-        txTypeAtt1 == "Deposit" || txTypeAtt1 == "Withdraw"
-          ? ""
-          : gpTimestamp,
+        txTypeAtt1 == "Deposit" || txTypeAtt1 == "Withdraw" ? "" : gpTimestamp,
       createdAt,
       usedPromo:
         txTypeAtt1 == "Deposit" || txTypeAtt1 == "Withdraw" ? "" : usedPromo,
@@ -215,9 +182,7 @@ async function processIon(ionRecord) {
           ? ""
           : promoWinReference,
       campaignType:
-        txTypeAtt1 == "Deposit" || txTypeAtt1 == "Withdraw"
-          ? ""
-          : campaignType,
+        txTypeAtt1 == "Deposit" || txTypeAtt1 == "Withdraw" ? "" : campaignType,
       campaignId:
         txTypeAtt1 == "Deposit" || txTypeAtt1 == "Withdraw" ? "" : campaignId,
       statistic,
@@ -233,26 +198,33 @@ async function processIon(ionRecord) {
 
     console.log("SQS: " + JSON.stringify(sqsMessage));
 
-    const sqsMessage1 = await sendMessage({
+    const sqsMessageStatistic = await sendMessage({
       url: StatisticUrl,
       payload: JSON.stringify(payload),
     });
 
-    console.log("SQS: " + JSON.stringify(sqsMessage1));
+    console.log("SQS: " + JSON.stringify(sqsMessageStatistic));
+
+    const sqsMessageBonusSystem = await sendMessage({
+      url: BonusSystemUrl,
+      payload: JSON.stringify(payload),
+    });
+
+    console.log("SQS: " + JSON.stringify(sqsMessageBonusSystem));
 
     // DDB Wager insertion
     // await insertOne(brandUsername, brandId, username, currentBalance, previousBalance, bonusCurrentBalance, bonusPreviousBalance, bonusAdjustAmount, adjustAmount, txType, txTypeAtt1, txTypeAtt2, txTypeAtt3, gameId, providerId, reference, roundDetails, roundId, gpTimestamp, createdAt, usedPromo, jackpotId);
-    const redisKey = 'pdc:' + brandUsername; 
+    const redisKey = "pdc:" + brandUsername;
     // selfmade optimistic lock :D
     const invoker = await redisCache(client, redisKey, createdAt);
-    console.log('data Redis:' + (createdAt < invoker));
+    console.log("data Redis:" + (createdAt < invoker));
 
     if (createdAt < invoker) {
-      return console.log('data update ke dynamo dan ws skipped');
+      return console.log("data update ke dynamo dan ws skipped");
     } else {
       // DDB updateBalance
-      await updateBalance(brandUsername, previousBalance, currentBalance);      
-      
+      await updateBalance(brandUsername, previousBalance, currentBalance);
+
       //start: update player balance websocket
       console.log(brandUsername);
       const response = await getWsConnectionId(brandUsername);
@@ -276,7 +248,7 @@ async function processIon(ionRecord) {
     // do an upsert so it doesn't matter if it is the initial version or not
     //await updateLicence(id, points, postcode, version);
   }
-};  
+}
 
 async function processRecords(records) {
   await Promise.all(
@@ -291,28 +263,28 @@ async function processRecords(records) {
       const b = JSON.parse(a);
 
       // Only process records where the record type is REVISION_DETAILS
-    //   if (ionRecord.recordType !== REVISION_DETAILS) {
-    //     debug(
-    //       `Skipping record of type ${dumpPrettyText(ionRecord.recordType)}`
-    //     );
-    //   } else {
-        debug(`Ion Record: ${dumpPrettyText(b)}`);
-        await processIon(b);
-    //   }
+      //   if (ionRecord.recordType !== REVISION_DETAILS) {
+      //     debug(
+      //       `Skipping record of type ${dumpPrettyText(ionRecord.recordType)}`
+      //     );
+      //   } else {
+      debug(`Ion Record: ${dumpPrettyText(b)}`);
+      await processIon(b);
+      //   }
     })
   );
-}  
+}
 
 module.exports.handler = async (event) => {
-    console.log(event)
-    try {
-        await Promise.all(
-            event.Records.map(async (kinesisRecord) => {
-            const records = await promiseDeaggregate(kinesisRecord.kinesis);
-            await processRecords(records);
-            })
-        );        
-    } catch (err) {
-        Promise.reject(`Kinesis Consumer Error: ${err}`);
-    }    
+  console.log(event);
+  try {
+    await Promise.all(
+      event.Records.map(async (kinesisRecord) => {
+        const records = await promiseDeaggregate(kinesisRecord.kinesis);
+        await processRecords(records);
+      })
+    );
+  } catch (err) {
+    Promise.reject(`Kinesis Consumer Error: ${err}`);
+  }
 };
